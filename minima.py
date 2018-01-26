d9d1c1f7eea3e2f68c6f28e5424ae19a70160fb0
IndiraPachon <indira197@gmail.com>
2018-01-26 16:07:53 -0500

    minima

---
+++ minima.py
@@ -0,0 +1,32 @@
+minima={} 
+for band in bands:
+    #Por cada banda, aplicar la máscara de nubes y cambiar el valor de nodata por np.nan
+    datos=np.where(np.logical_and(nbar.data_vars[band]!=nodata,cloud_mask),nbar.data_vars[band], np.nan)
+    allNan=~np.isnan(datos) #Una mascara que indica qué datos son o no nan. 
+    if normalized: #Normalizar, si es necesario.
+        #Para cada momento en el tiempo obtener el promedio y la desviación estándar de los valores de reflectancia
+        m=np.nanmean(datos.reshape((datos.shape[0],-1)), axis=1)
+        st=np.nanstd(datos.reshape((datos.shape[0],-1)), axis=1)
+        # usar ((x-x̄)/st) para llevar la distribución a media 0 y desviación estándar 1, 
+        # y luego hacer un cambio de espacio para la nueva desviación y media. 
+        datos=np.true_divide((datos-m[:,np.newaxis,np.newaxis]), st[:,np.newaxis,np.newaxis])*np.nanmin(st)+np.nanmin(m)
+    #Calcular la mediana en la dimensión de tiempo 
+    minima[band]=np.nanmin(datos,0) 
+    #Eliminar los valores que no cumplen con el número mínimo de pixeles válidos dado. 
+    minima[band][np.sum(allNan,0)<minValid]=np.nan
+del datos
+import xarray as xr
+ncoords=[]
+xdims =[]
+xcords={}
+for x in nbar.coords:
+    if(x!='time'):
+        ncoords.append( ( x, nbar.coords[x]) )
+        xdims.append(x)
+        xcords[x]=nbar.coords[x]
+variables ={k: xr.DataArray(v, dims=xdims,coords=ncoords)
+             for k, v in medians.items()}
+output=xr.Dataset(variables, attrs={'crs':nbar.crs})
+
+for x in output.coords:
+    output.coords[x].attrs["units"]=nbar.coords[x].units
